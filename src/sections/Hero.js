db9abe75e21c68519ff1c59c9cf2a54860aecddf
Sushil Jadhav <sushiljadhav407@gmail.com>
2026-04-29 16:49:51 +0530

    addeds

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (<section id="home" className="relative bg-[#f6f0e8] ">
       <div className="relative max-h-[180px] w-full md:max-h-[700px]">
-        <img src="/assets/zest-banner.png" alt="Modern co-living space" className="block h-full w-full object-cover"/>
+        <img src="/assets/zest-banner.webp" alt="Modern co-living space" className="block h-full w-full object-cover"/>
 
         
       </div>
