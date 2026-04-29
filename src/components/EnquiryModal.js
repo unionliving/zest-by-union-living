b3a599fcd5f6b0236ea0bd67006baa52ad57ea21
Sushil Jadhav <sushiljadhav407@gmail.com>
2026-04-29 11:52:31 +0530

    added attribute

--- src/components/EnquiryModal.js
+++ src/components/EnquiryModal.js
@@ -88,6 +88,7 @@ function EnquiryModal({ open, onClose }) {
         { Attribute: 'mx_Monthly_Budget',           Value: form.budget },
         { Attribute: 'Source',                      Value: 'Ruturaj Zest Landing Page' },
         { Attribute: 'SearchBy',                    Value: 'Phone' },
+        { Attribute: 'Ad Name' ,                    Value: 'Facebook - KP Launch Ad'},
       ];
 
       const { response, result } = await submitLeadSquaredLead(payload);
--- src/sections/SaveYourSpot.js
+++ src/sections/SaveYourSpot.js
@@ -41,6 +41,7 @@ const SaveYourSpot = () => {
         { Attribute: 'mx_Monthly_Budget', Value: form.budget },
         { Attribute: 'Source', Value: 'Ruturaj Zest Landing Page' },
         { Attribute: 'SearchBy', Value: 'Phone' },
+        { Attribute: 'Ad Name' , Value: 'Facebook - KP Launch Ad'},
       ];
 
       const { response, result } = await submitLeadSquaredLead(payload);
